0b1d53bd67e6525305f19dbbdb42e57393a9b5aa
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-04-15 00:17:36 +0200

    Create TestSuiteClass, testSuite test

--- TestCase.py
+++ TestCase.py
@@ -1,5 +1,3 @@
-from TestResult import TestResult
-
 
 class TestCase:
     def __init__(self, name):
@@ -8,8 +6,7 @@ class TestCase:
     def setUp(self):
         pass
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         self.setUp()
         try:
@@ -18,7 +15,6 @@ class TestCase:
         except:
             result.testFailed()
         self.tearDown()
-        return result
 
     def tearDown(self):
         pass
